9d02b0aede36a7bc15f8045070f491f2da4322aa
Uday Samsani <satyasaisamsani@gmail.com>
2019-11-18 20:41:19 +0530

    Home page

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,67 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core';
 
 const Blog = () => {
-	return (
-		<div>
-			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi
-			maxime aliquid provident modi excepturi dolore non numquam
-			praesentium laborum fugit suscipit, optio, nulla asperiores
-			reiciendis ducimus autem earum tempora quisquam aperiam a facilis
-			obcaecati similique rerum? Omnis dolore veritatis, ratione placeat
-			magni hic! Quos id fuga ab voluptatibus nam veritatis a. Laborum
-			debitis hic alias labore similique accusantium sit ut officia nam
-			consectetur distinctio accusamus eligendi, dolor omnis quo atque
-			earum, aliquam corrupti unde quisquam sunt eaque voluptatem aliquid?
-			Cumque minima qui officia voluptas quibusdam odio consequuntur totam
-			ducimus facere possimus. Consequatur provident illo fugit quod
-			exercitationem nulla architecto aut non quos cupiditate ad eum hic
-			dolor eaque dolorem nobis sapiente voluptates cumque pariatur
-			magnam, magni, dolore error minima. Totam ipsum ullam beatae,
-			explicabo corporis minima voluptatibus saepe tempore! Fugiat ad,
-			aliquid vero earum laboriosam totam. Minus magnam voluptatibus quam,
-			voluptate, non nisi totam eius molestias at itaque placeat facilis
-			deserunt! Ut nesciunt nihil ullam? Vero dolorem ipsum nostrum
-			tempora id corrupti quo? Iure iusto rem, dolorum obcaecati nulla qui
-			culpa expedita voluptatum error? Quisquam, doloribus eum excepturi,
-			laboriosam consectetur dolor ducimus adipisci architecto earum odit
-			aliquid fugiat nobis quod aperiam praesentium fuga, aliquam alias
-			sint consequatur recusandae repellat id mollitia laudantium.
-			Reprehenderit inventore temporibus perferendis blanditiis rem culpa
-			dicta eos illum modi, itaque, voluptatem obcaecati quisquam atque
-			voluptas totam harum ducimus voluptatum vel dolore est. Mollitia
-			officia eligendi impedit explicabo voluptatibus qui saepe, omnis,
-			praesentium suscipit delectus quibusdam sapiente magnam velit eum!
-			Et vitae exercitationem molestias reprehenderit vel in magni
-			corrupti. Aliquam mollitia et temporibus quo repellat odit inventore
-			unde atque, distinctio vel pariatur, maxime fuga. Pariatur
-			accusantium ex corrupti doloremque ratione! Quibusdam labore nam
-			ipsum sapiente fugiat numquam voluptates autem enim necessitatibus
-			dolore veniam porro obcaecati amet reiciendis ea alias, corrupti id
-			officiis nostrum! Reprehenderit laboriosam deserunt rerum nihil,
-			illo ex quae ab unde corrupti excepturi numquam error est impedit
-			velit facere in exercitationem itaque sint ipsam voluptate labore.
-			Assumenda numquam aperiam tempore beatae? Soluta quae ipsam eaque id
-			repellendus hic aspernatur maxime nisi! Magnam similique ipsum,
-			placeat, eaque nisi quaerat saepe, reiciendis excepturi aspernatur
-			provident aperiam temporibus soluta animi deleniti ducimus
-			voluptatum est. Suscipit vero esse dolorum aut impedit corrupti
-			alias pariatur, minima, sequi perferendis dicta, odit eum eaque sint
-			enim magnam iusto eos facere. Natus quo modi voluptas minima nostrum
-			ducimus adipisci similique, accusantium quisquam quis esse, fugit
-			aut et. Dolorum aliquam nemo autem nobis sequi delectus ex sapiente,
-			qui odio laborum nisi et facere. Iusto nam amet sunt ullam,
-			reiciendis commodi sit? Dolorem officiis voluptates laudantium
-			labore maxime doloribus quidem totam voluptate esse, optio quia nisi
-			facilis quas temporibus quod sapiente provident. Molestiae adipisci
-			quas illum at alias culpa sed facilis cupiditate nulla, eum atque
-			saepe beatae dolores cum vitae ad non blanditiis harum mollitia est
-			consectetur corrupti magnam? Est eligendi laudantium architecto eum
-			totam tempora labore odit earum reiciendis, ipsum, quam atque animi
-			odio numquam quasi voluptatibus eveniet illum, unde veniam aliquid?
-			Adipisci vitae debitis omnis, facilis quas itaque! Earum quas
-			ducimus unde sunt?
-		</div>
-	);
+	return <></>;
 };
 
 export default Blog;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,82 @@
 import React from 'react';
+import { Typography, Container, makeStyles, Grid } from '@material-ui/core';
+
+import udayImage from '../assets/images/profileImage.jpg';
+
+const useStyles = makeStyles(theme => ({
+	head: {
+		textAlign: 'center',
+		padding: '15rem 0'
+	},
+	about: {
+		backgroundColor: '#0375b4',
+		padding: '2rem 0',
+		textAlign: 'center'
+	},
+	title: {
+		color: '#ffce00',
+		fontWeight: 800,
+		margin: '1rem 0'
+	},
+	content: {
+		color: '#0e0b16',
+		fontSize: '1.5rem',
+		padding: '1rem 3rem',
+		textAlign: 'center'
+	},
+	footer: {
+		color: '#fff',
+		backgroundColor: '#373737',
+		fontSize: '1rem',
+		padding: '2rem'
+	}
+}));
 
 const Home = () => {
-	return <div>This is Home.</div>;
+	const classes = useStyles();
+	return (
+		<>
+			<div className={classes.head}>
+				<Typography variant='h1' className={classes.title}>
+					<span>Learn</span> <span>Code</span> <span>Enjoy</span>{' '}
+					<span>Repeat</span>{' '}
+				</Typography>
+			</div>
+			<div className={classes.about}>
+				<Container>
+					<Typography variant='h3' className={classes.title}>
+						About
+					</Typography>
+					<Grid container className='aboutSection' spacing={3}>
+						<Grid item sm='6'>
+							<Typography variant='p' className={classes.content}>
+								I am Uday Samsani. I am from vijayawada and I am
+								doing my graduation as of now. I love to learn
+								new things. I have many hobbies which are
+								watching movies, reading books, surfing
+								internet, playing games (especially DOTA) and
+								listening to music.
+							</Typography>
+						</Grid>
+						<Grid item sm='6'>
+							<img
+								src={udayImage}
+								alt='Uday Samsani'
+								width={250}
+							/>
+						</Grid>
+					</Grid>
+				</Container>
+			</div>
+			<div className={classes.footer}>
+				<Container>
+					<Typography variant={'p'}>
+						Copyrights udaysamsani.com, 2019.
+					</Typography>
+				</Container>
+			</div>
+		</>
+	);
 };
 
 export default Home;
