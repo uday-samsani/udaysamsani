ca690aad61b1f9799e4826f44a4d0c7343e46ffe
Uday Samsani <satyasaisamsani@gmail.com>
2019-11-18 20:30:19 +0530

    Nav Bar changed & ui changed to material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
 
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
-import About from './pages/About';
+import Projects from './pages/Projects';
 import Login from './pages/Login';
 
-import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 // TODO: need to degign home page
@@ -16,13 +14,11 @@ import './App.css';
 function App() {
 	return (
 		<Router>
-			<NavBar className='NavBar' />
-			<Container className='Page'>
-				<Route exact path='/' component={Home} />
-				<Route path='/Blog' component={Blog} />
-				<Route path='/about' component={About} />
-				<Route path='/login' component={Login} />
-			</Container>
+			<NavBar />
+			<Route exact path='/' component={Home} />
+			<Route path='/Blog' component={Blog} />
+			<Route path='/projects' component={Projects} />
+			<Route path='/login' component={Login} />
 		</Router>
 	);
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,152 +1,128 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Menu, Container, Responsive, Icon, Sidebar } from 'semantic-ui-react';
+import {
+	makeStyles,
+	AppBar,
+	Toolbar,
+	Typography,
+	IconButton
+} from '@material-ui/core';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
-import './css/NavBar.css';
+const useStyles = makeStyles(theme => ({
+	navBar: {
+		backgroundColor: '#373737'
+	},
+	menuButton: {
+		marginRight: '1rem',
+		padding: '0.5rem',
+		backgroundColor: '#fff',
+		color: '#000',
+		marginBottom: 7
+	},
+	title: {
+		fontFamily: 'Barlow',
+		fontWeight: 600,
+		padding: 'auto',
+		color: '#fff',
+		paddingRight: '1rem',
+		textDecoration: 'none'
+	},
+	item: {
+		fontFamily: 'Barlow',
+		fontWeight: 500,
+		color: '#fff',
+		textDecoration: 'none',
+		padding: '0.5rem',
+		margin: '0 0.5rem'
+	},
+	activeItem: {
+		padding: '0.5rem',
+		borderBottom: '2px solid #fff'
+	},
+	navLeft: {
+		flex: 1,
+		padding: 'auto'
+	},
+	navRight: {
+		flex: 1,
+		padding: 'auto'
+	}
+}));
 
-const NavBar = () => {
+export default function NavBar() {
+	const classes = useStyles();
+
 	const pathname = window.location.pathname;
-	const path = pathname === '/' ? 'home' : pathname.substr(1);
-
-	const [activeItem, setActiveItem] = useState(path);
-	const [sideBar, setSideBar] = useState(false);
-
-	const handleItemClick = (e, { name }) => {
-		setActiveItem(name);
-		if (sideBar) {
-			setSideBar(false);
-		}
+	const [activeItem, setActiveItem] = useState(pathname.split('/')[2]);
+	const handleLinkClick = event => {
+		const href = event.target.href.split('/');
+		const path = href[href.length - 1];
+		setActiveItem(path);
 	};
-	const handleIconClick = (e, data) => setSideBar(!sideBar);
+
 	return (
-		<>
-			<Sidebar
-				as={Menu}
-				animation='overlay'
-				icon='labeled'
-				inverted
-				onHide={() => setSideBar(false)}
-				vertical
-				visible={sideBar}
-				width='thin'
-			>
-				<Menu.Item onClick={handleIconClick}>
-					<p className='NavCloseMobile'>
-						<Icon className='NavIconMobile' name='close' fitted />{' '}
-						Close
-					</p>
-				</Menu.Item>
-				<Menu.Item
-					name='home'
-					active={activeItem === 'home'}
-					onClick={handleItemClick}
-					as={Link}
-					to={'/'}
+		<AppBar position='static' className={classes.navBar} elevation={0}>
+			<Toolbar>
+				<Typography
+					variant='h5'
+					className={classes.title}
+					component={Link}
+					to='/'
+					onClick={handleLinkClick}
 				>
-					<p className='NavItemMobile'>Home</p>
-				</Menu.Item>
-				<Menu.Item
-					name='blog'
-					active={activeItem === 'blog'}
-					onClick={handleItemClick}
-					as={Link}
-					to={'/blog'}
-				>
-					<p className='NavItemMobile'>Blog</p>
-				</Menu.Item>
-				<Menu.Item
-					name='about'
-					active={activeItem === 'about'}
-					onClick={handleItemClick}
-					as={Link}
-					to={'/about'}
-				>
-					<p className='NavItemMobile'>About</p>
-				</Menu.Item>
-
-				<Menu.Item
-					name='login'
-					active={activeItem === 'login'}
-					onClick={handleItemClick}
-					as={Link}
-					to={'/login'}
-				>
-					<p className='NavItemMobile'>Login</p>
-				</Menu.Item>
-			</Sidebar>
-			<Container textAlign='center'>
-				<Menu text size='massive' color='teal' className='NavBar'>
-					<Responsive
-						as={Menu.Menu}
-						position='left'
-						minWidth={Responsive.onlyMobile.maxWidth}
+					udaysamsani.com
+				</Typography>
+				<div className={classes.navRight}>
+					<Typography
+						variant='h6'
+						className={[
+							classes.item,
+							activeItem === 'blog' ? classes.activeItem : null
+						]}
+						component={Link}
+						to='/blog'
+						onClick={handleLinkClick}
 					>
-						<Menu.Item
-							name='home'
-							active={activeItem === 'home'}
-							onClick={handleItemClick}
-							as={Link}
-							to={'/'}
-						>
-							<p className='NavItem'>Home</p>
-						</Menu.Item>
-						<Menu.Item
-							name='blog'
-							active={activeItem === 'blog'}
-							onClick={handleItemClick}
-							as={Link}
-							to={'/blog'}
-						>
-							<p className='NavItem'>Blog</p>
-						</Menu.Item>
-					</Responsive>
-					<Menu.Menu>
-						<Responsive
-							as={Menu.Item}
-							position='left'
-							maxWidth={Responsive.onlyMobile.maxWidth}
-							onClick={handleIconClick}
-						>
-							<Icon
-								className='NavIcon'
-								name='bars'
-								size='large'
-								// fitted
-							/>
-						</Responsive>
-						<Menu.Item className='NavItem' position='left'>
-							<p className='NavTitle'>UDAY SAMSANI</p>
-						</Menu.Item>
-					</Menu.Menu>
-					<Responsive
-						as={Menu.Menu}
-						position='left'
-						minWidth={Responsive.onlyMobile.maxWidth}
+						Blog
+					</Typography>
+					<Typography
+						variant='h6'
+						className={[
+							classes.item,
+							activeItem === 'projects'
+								? classes.activeItem
+								: null
+						]}
+						component={Link}
+						to='/projects'
+						onClick={handleLinkClick}
 					>
-						<Menu.Item
-							name='about'
-							active={activeItem === 'about'}
-							onClick={handleItemClick}
-							as={Link}
-							to={'/about'}
-						>
-							<p className='NavItem'>About</p>
-						</Menu.Item>
-
-						<Menu.Item
-							name='login'
-							active={activeItem === 'login'}
-							onClick={handleItemClick}
-							as={Link}
-							to={'/login'}
-						>
-							<p className='NavItem'>Login</p>
-						</Menu.Item>
-					</Responsive>
-				</Menu>
-			</Container>
-		</>
+						Projects
+					</Typography>
+				</div>
+				<div className='classes.navLeft'>
+					<IconButton
+						className={classes.menuButton}
+						color='inherit'
+						aria-label='menu'
+					>
+						<MoreHorizIcon />
+					</IconButton>
+					<Typography
+						variant='h6'
+						className={[
+							classes.item,
+							activeItem === 'login' ? classes.activeItem : null
+						]}
+						component={Link}
+						to='/login'
+						onClick={handleLinkClick}
+					>
+						Login
+					</Typography>
+				</div>
+			</Toolbar>
+		</AppBar>
 	);
-};
-
-export default NavBar;
+}
