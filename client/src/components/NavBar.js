39050f8b998719765bd4d20f9711f0b6678a4796
Uday Samsani <satyasaisamsani@gmail.com>
2019-12-20 01:39:02 +0530

    NavBar created unpolished

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Box, Heading, Header, Image } from 'grommet';
 
-export default function NavBar() {
+import logo from '../assets/icons/logo.png';
+import './css/NavBar.css';
+
+const NavBar = () => {
 	const pathname = window.location.pathname;
 	const [activeItem, setActiveItem] = useState(pathname.split('/')[2]);
 	const handleLinkClick = event => {
@@ -10,5 +14,45 @@ export default function NavBar() {
 		setActiveItem(path);
 	};
 
-	return <h1>Hello Nav Bar here</h1>;
-}
+	return (
+		<Header>
+			<Box direction='row' pad='small'>
+				<Box direction='row' width='xsmall' height='xsmall' pad='small'>
+					<Image src={logo} fit={'contain'} />
+				</Box>
+				<Box direction='row' pad='small' gap='medium' align='start'>
+					<Link
+						to='/'
+						style={{ textDecoration: 'none', color: '#000000' }}
+					>
+						<Heading level={3}>udaysamsani.com</Heading>
+					</Link>
+					<Link
+						to='/blog'
+						style={{ textDecoration: 'none', color: '#000000' }}
+					>
+						<Heading level={3}>blog</Heading>
+					</Link>
+					<Link
+						to='/projects'
+						style={{ textDecoration: 'none', color: '#000000' }}
+					>
+						<Heading level={3}>project</Heading>
+					</Link>
+				</Box>
+			</Box>
+			<Box direction='row' pad='small'>
+				<Box direction='row' pad='small' gap='medium'>
+					<Link
+						to='/login'
+						style={{ textDecoration: 'none', color: '#000000' }}
+					>
+						<Heading level={3}>login</Heading>
+					</Link>
+				</Box>
+			</Box>
+		</Header>
+	);
+};
+
+export default NavBar;
