9944cbd1d6d71bdd63f6fc44adfbffe17acf4afa
UdaySamsani <satyasaisamsani@gmail.com>
2020-04-23 13:03:16 +0530

    Post fetch queries modiied.

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,71 +1,72 @@
 const gql = require('graphql-tag');
 
 const typeDefs = gql`
-	type User {
-		_id: ID!
-		username: String
-		email: String
-		token: String
-		dob: String
-		createdAt: String
-		updatedAt: String
-	}
-	type Comment {
-		_id: ID!
-		user: User!
-		body: String!
-		createdAt: String!
-		updatedAt: String
-	}
-	type Like {
-		_id: ID!
-		user: User!
-		createdAt: String!
-	}
-	type Post {
-		_id: ID!
-		title: String!
-		subtitle: String
-		body: String!
-		user: User
-		tags: [String]
-		comments: [Comment]!
-		likes: [Like]!
-		createdAt: String!
-		updatedAt: String
-	}
-	input SigninInput {
-		username: String!
-		email: String!
-		password: String!
-		confirmPassword: String!
-		dob: String!
-	}
-	input PostInput {
-		title: String!
-		subtitle: String
-		body: String!
-		tags: [String]
-	}
-	type Query {
-		sayHi: String!
-		getPost(postId: ID!): Post!
-		getPosts: [Post]!
-	}
-	type Mutation {
-		login(username: String!, password: String!): User!
-		signin(signinInput: SigninInput): User!
+    type User {
+        _id: ID!
+        username: String
+        email: String
+        token: String
+        dob: String
+        createdAt: String
+        updatedAt: String
+    }
+    type Comment {
+        _id: ID!
+        user: User!
+        body: String!
+        createdAt: String!
+        updatedAt: String
+    }
+    type Like {
+        _id: ID!
+        user: User!
+        createdAt: String!
+    }
+    type Post {
+        _id: ID!
+        title: String!
+        subtitle: String
+        body: String!
+        user: User
+        tags: [String]
+        comments: [Comment]!
+        likes: [Like]!
+        createdAt: String!
+        updatedAt: String
+    }
+    input SigninInput {
+        username: String!
+        email: String!
+        password: String!
+        confirmPassword: String!
+        dob: String!
+    }
+    input PostInput {
+        title: String!
+        subtitle: String
+        body: String!
+        tags: [String]
+    }
+    type Query {
+        sayHi: String!
+        getPostById(postId: ID!): Post!
+        getPostByTitle(postTitle: String!): Post!
+        getPosts: [Post]!
+    }
+    type Mutation {
+        login(username: String!, password: String!): User!
+        signin(signinInput: SigninInput): User!
 
-		createPost(postInput: PostInput): Post!
-		updatePost(postId: ID!, postInput: PostInput): Post!
-		deletePost(postId: ID!): String!
+        createPost(postInput: PostInput): Post!
+        updatePost(postId: ID!, postInput: PostInput): Post!
+        deletePost(postId: ID!): String!
 
-		createComment(postId: ID!, body: String!): Post!
-		updateComment(commentId: ID!, body: String!): Post!
-		deleteComment(commentId: ID!): Post!
+        createComment(postId: ID!, body: String!): Post!
+        updateComment(commentId: ID!, body: String!): Post!
+        deleteComment(commentId: ID!): Post!
 
-		likePost(postId: ID!): Post!
-	}
+        likePost(postId: ID!): Post!
+    }
 `;
 
 module.exports = typeDefs;
--- utils/validators.js
+++ utils/validators.js
@@ -1,85 +1,85 @@
 const User = require('../models/User');
 const Post = require('../models/Post');
 module.exports.validateSigninInput = async (
-	username,
-	email,
-	password,
-	confirmPassword,
-	dob
+    username,
+    email,
+    password,
+    confirmPassword,
+    dob
 ) => {
-	const errors = {};
-	if (username.trim() === '') {
-		errors.username = 'username must not be empty';
-	} else {
-		const user = await User.findOne({ username });
-		if (user) {
-			errors.username = 'username is already taken';
-		}
-	}
+    const errors = {};
+    if (username.trim() === '') {
+        errors.username = 'username must not be empty';
+    } else {
+        const user = await User.findOne({ username });
+        if (user) {
+            errors.username = 'username is already taken';
+        }
+    }
 
-	if (dob.trim() === '') {
-		errors.dob = 'date of birth must not be empty';
-	}
-	if (email.trim() === '') {
-		errors.email = 'email must not be empty';
-	} else {
-		const user = await User.findOne({ email });
-		if (user) {
-			errors.email = 'email linked to another account';
-		} else {
-			const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-			if (!email.match(regEx)) {
-				errors.email = 'email must be a valid email address';
-			}
-		}
-	}
-	if (password === '') {
-		email.password = 'password must not be empty';
-	} else if (password !== confirmPassword) {
-		errors.password = 'passwords do not match';
-	}
-	return {
-		errors,
-		valid: Object.keys(errors).length < 1
-	};
+    if (dob.trim() === '') {
+        errors.dob = 'date of birth must not be empty';
+    }
+    if (email.trim() === '') {
+        errors.email = 'email must not be empty';
+    } else {
+        const user = await User.findOne({ email });
+        if (user) {
+            errors.email = 'email linked to another account';
+        } else {
+            const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+            if (!email.match(regEx)) {
+                errors.email = 'email must be a valid email address';
+            }
+        }
+    }
+    if (password === '') {
+        email.password = 'password must not be empty';
+    } else if (password !== confirmPassword) {
+        errors.password = 'passwords do not match';
+    }
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1,
+    };
 };
 
 module.exports.validateLoginInput = (username, password) => {
-	const errors = {};
-	if (username.trim() === '') {
-		errors.username = 'username must not be empty';
-	}
-	if (password === '') {
-		errors.password = 'password must not be empty';
-	}
-	return {
-		errors,
-		valid: Object.keys(errors).length < 1
-	};
+    const errors = {};
+    if (username.trim() === '') {
+        errors.username = 'username must not be empty';
+    }
+    if (password === '') {
+        errors.password = 'password must not be empty';
+    }
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1,
+    };
 };
 
 module.exports.validatePostInput = async (title, body) => {
-	const errors = {};
-	if (title.trim() === '') {
-		errors.title = 'title must not be empty';
-	} else {
-		const post = await Post.findOne({ title: title });
-		if (post) {
-			errors.title = 'title must be unique';
-		}
-	}
-	if (body.trim() === '') {
-		errors.body = 'body must not be empty';
-	}
-	return {
-		errors,
-		valid: Object.keys(errors).length < 1
-	};
+    const errors = {};
+    if (title.trim() === '') {
+        errors.title = 'title must not be empty';
+    } else {
+        const post = await Post.findOne({ title: title });
+        if (post) {
+            errors.title = 'title must be unique';
+        }
+    }
+    if (body.trim() === '') {
+        errors.body = 'body must not be empty';
+    }
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1,
+    };
 };
-module.exports.validateCommentInput = body => {
-	const errors = {};
-	if (body.trim() === '') {
-		errors.body = 'body must not be empty';
-	}
-	return { errors, valid: Object.keys(errors).length < 1 };
+module.exports.validateCommentInput = (body) => {
+    const errors = {};
+    if (body.trim() === '') {
+        errors.body = 'body must not be empty';
+    }
+    return { errors, valid: Object.keys(errors).length < 1 };
 };
