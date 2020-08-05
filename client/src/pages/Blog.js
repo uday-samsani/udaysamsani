7a3803bd368977a7c08c0a685434ca877ba4795e
UdaySamsani <satyasaisamsani@gmail.com>
2020-08-06 01:02:03 +0530

    Updated Blog, Home, Post

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
-import { Box, Button, CircularProgress, Typography } from '@material-ui/core';
+import {
+    Box,
+    Button,
+    Container,
+    CircularProgress,
+    Typography,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { Add } from '@material-ui/icons';
 
+import { AuthContext } from '../context/auth';
 import { FETCH_POSTS_QUERY } from '../utils/Graphql';
 
 import Card from '../components/Card';
@@ -19,6 +27,8 @@ const useStyles = makeStyles((theme) => ({
     },
     heading: {
         flexGrow: 1,
+        fontWeight: '500',
+        textAlign: 'center',
     },
     link: {
         textDecoration: 'none',
@@ -31,15 +41,21 @@ const Blog = () => {
     const { loading, data } = useQuery(FETCH_POSTS_QUERY, {
         fetchPolicy: 'cache-and-network',
     });
+    const { user } = useContext(AuthContext);
+
     return (
-        <Box className={classes.root}>
+        <Container className={classes.root} maxWidth='md'>
             <Box className={classes.box}>
                 <Typography variant='h4' className={classes.heading}>
                     Blog
                 </Typography>
-                <Link to='/createBlog' className={classes.link}>
-                    <Button variant={'outlined'}>Publish Blog</Button>
-                </Link>
+                {user ? (
+                    <Link to='/createBlog' className={classes.link}>
+                        <Button variant={'outlined'} startIcon={<Add />}>
+                            Blog
+                        </Button>
+                    </Link>
+                ) : null}
             </Box>
             {!loading ? (
                 data.getPosts.map((post, index) => {
@@ -48,7 +64,7 @@ const Blog = () => {
             ) : (
                 <CircularProgress color='secondary' />
             )}
-        </Box>
+        </Container>
     );
 };
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,30 @@
 import React from 'react';
 
-import { Grid, Typography, useMediaQuery } from '@material-ui/core';
+import { Container, Grid, Typography, useMediaQuery } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Typed from 'react-typed';
 
 import HomeIllustration from '../images/Blogging.gif';
+import Hand from '../images/hand.svg';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         padding: '2em 0',
     },
-    title: {
-        fontWeight: 'bold',
+    description: {
+        backgroundColor: 'red',
     },
-    text: {
-        padding: ' 1em 2em',
-        textAlign: 'center',
+    text: { padding: '1em', textAlign: 'center' },
+    wave: {
+        marginRight: 3,
+        animationName: 'wave-animation',
+        animationDuration: '2.5s',
+        animationIterationCount: 'infinite',
+        transformOrigin: '70% 70%',
+        display: 'inline-block',
+    },
+    contact: {
+        backgroundColor: '#efefef',
     },
 }));
 
@@ -23,34 +33,54 @@ const Home = (props) => {
     const isMobile = useMediaQuery((theme) => theme.breakpoints.down('sm'));
     return (
         <div className={classes.root}>
-            <Grid
-                container
-                wrap='wrap'
-                cols={2}
-                alignItems='center'
-                // justifyItems='space-evenly'
-            >
-                <Grid item>
-                    <img
-                        src={HomeIllustration}
-                        alt={'Coding Illustration'}
-                        width={isMobile ? '350px' : '600px'}
-                    />
+            <Container>
+                <Grid
+                    container
+                    cols={2}
+                    justify={isMobile ? 'center' : 'flex-start'}
+                    alignItems='center'
+                >
+                    <Grid item>
+                        <img
+                            src={HomeIllustration}
+                            alt={'Coding Illustration'}
+                            width={isMobile ? '350px' : '600px'}
+                        />
+                    </Grid>
+                    <Grid item xs>
+                        <Typography variant='h4' className={classes.text} wrap>
+                            {'I’m Uday Samsani '}
+                            <img
+                                src={Hand}
+                                width='35px'
+                                height='35px'
+                                className={classes.wave}
+                            />
+                            {','}
+                            {'I code and design websites and loves '}
+                            <Typed
+                                strings={[
+                                    'learning',
+                                    'gaming',
+                                    'photography',
+                                    'editing',
+                                    'exploring',
+                                ]}
+                                typeSpeed={60}
+                                backSpeed={60}
+                                loop
+                            />
+                        </Typography>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <div className={classes.text}>
-                        <Typography variant='h4' className={classes.title}>
-                            {'Coffee + Code'}
-                        </Typography>
-                        <Typography variant='h5'>
-                            A way to deal JavaScript.
-                        </Typography>
-                        <Typography variant='h6'>
-                            Been developing web applications since 2 years.
-                        </Typography>
-                    </div>
-                </Grid>
-            </Grid>
+            </Container>
+            <div className={classes.contact}>
+                <Container>
+                    <Typography variant='h7'>
+                        Email: satyasaisamsani@gmail.com
+                    </Typography>
+                </Container>
+            </div>
         </div>
     );
 };
