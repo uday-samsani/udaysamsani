bde17ad838e71fb643bc99eba01681f11c733e86
UdaySamsani <satyasaisamsani@gmail.com>
2020-08-11 02:27:55 +0530

    Added Card grid

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -4,10 +4,10 @@ import moment from 'moment';
 import clsx from 'clsx';
 
 import {
+    Box,
     Button,
     Card,
     CardActions,
-    CardActionArea,
     CardContent,
     CardMedia,
     Typography,
@@ -16,44 +16,60 @@ import {
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        maxWidth: 345,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignContent: 'space-betweeen',
+        width: 290,
+        height: 300,
         backgroundColor: '#eeeeee',
     },
     media: {
         height: 140,
     },
-    mediaClose: {
-        transition: theme.transitions.create('height', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        height: 0,
-        [theme.breakpoints.up('sm')]: {
-            height: 0,
-        },
+    description: {
+        marginTop: -90,
+    },
+    link: {
+        textDecoration: 'none',
+        color: 'inherit',
     },
 }));
 
 const BlogCard = ({ post }) => {
+    const [elevation, setElevation] = useState(0);
     const classes = useStyles();
-    const description = post.body.replace(/<[^>]+>/g, '').split(' ', 25);
+    const description = post.body.replace(/<[^>]+>/g, '').split(' ', 40);
     const [readDescription, setReadDescription] = useState(false);
 
-    const handleReadDescription = (e) => {
-        setReadDescription(!readDescription);
+    const handleMouseOver = () => {
+        setReadDescription(true);
+        setElevation(3);
+    };
+    const handleMouseOut = () => {
+        setReadDescription(false);
+        setElevation(0);
     };
 
     return (
-        <Card className={classes.root} elevation={0}>
-            <CardActionArea onClick={handleReadDescription}>
+        <Card
+            className={classes.root}
+            elevation={elevation}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+        >
+            <Box
+                className={
+                    post.coverImage
+                        ? readDescription
+                            ? classes.description
+                            : null
+                        : null
+                }
+            >
                 {post.coverImage ? (
                     <CardMedia
-                        className={
-                            !readDescription
-                                ? classes.media
-                                : clsx(classes.media, classes.mediaClose)
-                        }
+                        className={classes.media}
                         image={
                             'https://storage.googleapis.com/uday-samsani/' +
                             post.coverImage
@@ -62,22 +78,34 @@ const BlogCard = ({ post }) => {
                     />
                 ) : null}
                 <CardContent>
-                    <Typography gutterBottom variant='h5' component='h2'>
-                        {post.title}
-                    </Typography>
+                    <Link
+                        to={`/blog/${post.title.trim().replace(/ /g, '-')}`}
+                        className={classes.link}
+                    >
+                        <Typography gutterBottom variant='h5' component='h2'>
+                            {post.title}
+                        </Typography>
+                    </Link>
                     <Typography
                         variant='body2'
                         color='textSecondary'
                         component='p'
                     >
-                        {!readDescription
-                            ? description.slice(0, 13).join(' ') + ' ...'
+                        {!post.coverImage
+                            ? description.join(' ')
+                            : !readDescription
+                            ? description.slice(0, 14).join(' ') + ' ...'
                             : description.join(' ')}
                     </Typography>
                 </CardContent>
-            </CardActionArea>
+            </Box>
             <CardActions>
-                <Button size='small'>Read More</Button>
+                <Link
+                    to={`/blog/${post.title.trim().replace(/ /g, '-')}`}
+                    className={classes.link}
+                >
+                    <Button size='small'>Read More</Button>
+                </Link>
             </CardActions>
         </Card>
     );
