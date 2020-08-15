73249122277533c39285a21957ea76fbbbd2114e
UdaySamsani <satyasaisamsani@gmail.com>
2020-08-15 23:15:17 +0530

    Fixed path import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { path } = require('path');
+const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 
 const connectDB = require('./config/db');
