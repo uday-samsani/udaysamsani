13542de8c5086970b429a01ecb1f56354e71f557
UdaySamsani <satyasaisamsani@gmail.com>
2020-08-17 16:14:33 +0530

    Updated sender name

--- utils/email.js
+++ utils/email.js
@@ -7,7 +7,7 @@ const sendVerificationMail = async ({ user, token }) => {
             ? 'https://udaysamsani.codes/verify/' + token
             : 'http://localhost:3000/verify/' + token;
     const mail = {
-        from: 'UdaySamsani <no-reply@udaysamsani.codes>',
+        from: 'Uday Samsani <no-reply@udaysamsani.codes>',
         to: user.email,
         subject: '[Uday Samsani] Please verify your email address',
         templateId: 'd-59cdaea053fd4c6ca4a33c56fa2fed15',
@@ -31,7 +31,7 @@ const sendPasswordResetMail = async ({ user, token }) => {
             ? 'https://udaysamsani.codes/password-reset/' + token
             : 'http://localhost:3000/password-reset/' + token;
     const mail = {
-        from: 'UdaySamsani <no-reply@udaysamsani.codes>',
+        from: 'Uday Samsani <no-reply@udaysamsani.codes>',
         to: user.email,
         subject: '[Uday Samsani] Please reset your password',
         templateId: 'd-ae6ec4386c2745c9ac5bab740a5d1787',
