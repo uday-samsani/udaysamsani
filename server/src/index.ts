78559299a54db64e1ccf5fd20c5fa230cc0afce8
Uday Samsani <satyasaisamsani@gmail.com>
2021-03-28 10:48:46 +0530

    Modified: Typeorm config and migrations runner

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,19 +4,34 @@ import mercurius from 'mercurius';
 import {buildSchema} from 'type-graphql';
 import UserResolvers from './resolvers/user';
 import {createConnection} from 'typeorm';
+import User from './entities/User';
+import * as path from 'path';
 
 const main = async () => {
     const app = Fastify({logger: true});
 
     try{
-        await createConnection();
+        await createConnection({
+            type: "postgres",
+            host: "localhost",
+            port: 5432,
+            username: "postgres",
+            password: "0mega99@2018",
+            database: "udaysamsani",
+            logging: true,
+            migrations: [path.join(__dirname, "./migrations/*")],
+            entities: [User],
+        });
+        // await User.delete({})
+        // await getConnection().runMigrations()
         app.log.info("Database connection successful")
     } catch (err){
         app.log.error("Database connection failed :"+err.message)
     }
 
     const schema = await buildSchema({
-        resolvers: [UserResolvers]
+        resolvers: [UserResolvers],
+        validate: false
     });
     app.register(mercurius, {schema, graphiql: 'playground', path: '/graphql'});
     const port: number = parseInt(process.env.port || '') || 4000;
